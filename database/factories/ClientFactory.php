eddd6677d8e6754ca49dc1a598ab5d27de2432e7
Osama Eltayar <osama.eltayar@areebgroup.com>
2025-05-02 19:35:38 +0300

    remove unneeded

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Enums\ClientStatus;
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,6 +11,8 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class ClientFactory extends Factory
 {
+    protected $model = Client::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +21,31 @@ class ClientFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name_ar' => $this->faker->name(),
+            'name_en' => $this->faker->name(),
+            'national_number' => $this->faker->unique()->numerify('##########'),
+            'passport_number' => $this->faker->unique()->bothify('??#######'),
+            'date_of_birth' => $this->faker->dateTimeBetween('-60 years', '-18 years'),
+            'parent_id' => null,
+            'status' => $this->faker->randomElement(ClientStatus::cases()),
         ];
+    }
+
+    public function withParent(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'parent_id' => Client::factory(),
+            ];
+        });
+    }
+
+    public function withChildren(): self
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'parent_id' => null,
+            ];
+        });
     }
 }
